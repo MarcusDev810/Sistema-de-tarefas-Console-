c2c86b1cdca34e19773734c4d4129971fd67f54c
Marcus Vinícius <marcus.vinicius@sga.pucminas.br>
2026-03-05 20:05:39 -0300

    Adição da função Tamanho e Adicionar_Inicio

--- ListaEstatica.cs
+++ ListaEstatica.cs
@@ -9,6 +9,24 @@ class ListaEstatica{
         this.Tam = 0;
         this.Tam_Max = 1000;
         this.tarefa = new Tarefas[Tam_Max];
+    }
 
+    public int Tamanho(){
+        return Tam;
+                
+    }
+    
+    public void Adicionar_Inicio(string nome, string descricao, int importancia){
+        if(Tam == Tam_Max){
+            return;
+        }
+
+        for(int i = Tam; i > 0; i--){
+            tarefa[i] = tarefa[i - 1];
+            
+        }
+
+        tarefa[0] = new Tarefas(nome, descricao, importancia);
+        Tam++;
     }
 }
