78ce102d5c4eaa0b118ad22e5a14f379f8405f6d
Marcus Vinicius <marcusvini810.dev@gmail.com>
2026-03-16 20:58:20 -0300

    Começo da main para a 'cara' do projeto

--- Program.cs
+++ Program.cs
@@ -4,7 +4,25 @@ class Program{
     static void Main(){
 
         ListaEstatica lista = new ListaEstatica();
+        int opcao = 0;
+
+
+
+        opcao = Menu();
 
         
+    }
+
+    static int Menu(){
+        Console.WriteLine("====> ESTOQUE FÁCIL <====");
+        Console.WriteLine("1 - Listar suas tarefas");
+        Console.WriteLine("2 - Adiciona uma tarefa");
+        Console.WriteLine("3 - Concluir uma tarefa");
+        Console.WriteLine("4 - Remover uma tarefa");
+        Console.WriteLine("5 - Buscar tarefa por nome");
+        Console.WriteLine("6 - Buscar tarefas da mesma importância");
+        Console.WriteLine("0 - Sair");
+        Console.WriteLine("Digite a opção desejada:");
+
     }
 }
